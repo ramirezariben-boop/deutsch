54e2cb61befa5ace65802c3c4c67376e7cd830ae
AriiBen <ramirez.ariben@gmail.com>
2026-02-08 09:08:57 -0600

    fix classroom link2

--- app/schueler/SchuelerClient.tsx
+++ app/schueler/SchuelerClient.tsx
@@ -33,7 +33,7 @@ export default function SchuelerClient({ alumno }: { alumno: Alumno }) {
           </a>
 
           <a
-            href="https://classroom.google.com/c/NzkyODQ5MTgyOTkx?cjc=yuqnyyqw"
+            href="https://classroom.google.com/c/NzkyODQ5MTgyOTkx"
             target="_blank"
             rel="noopener noreferrer"
             className="px-6 py-2 bg-slate-600 hover:bg-slate-700 rounded text-center w-56"

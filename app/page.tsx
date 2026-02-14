d3708c9e3154942f4e704b277f551d64b47f8557
AriiBen <ramirez.ariben@gmail.com>
2026-02-14 14:46:32 -0600

    add right lyrickahoot link to frontend

--- app/page.tsx
+++ app/page.tsx
@@ -47,7 +47,7 @@ export default async function MainPage() {
           </a>
 
           <a
-            href="https://lyrickahoot.ariiben.com"
+            href="https://lyrickahoot.ariiben.com/game/player.html"
             className="px-6 py-2 bg-slate-600 hover:bg-slate-700 rounded text-center w-56"
           >
             🎶 Lyrickahoot! 🧩

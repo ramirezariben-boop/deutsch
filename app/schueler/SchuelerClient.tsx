455c1e457495e34de49f0f6748ada3259f63ce14
AriiBen <ramirez.ariben@gmail.com>
2026-02-20 12:09:46 -0600

    schreiben

--- app/schueler/SchuelerClient.tsx
+++ app/schueler/SchuelerClient.tsx
@@ -67,6 +67,17 @@ export default function SchuelerClient({ alumno }: { alumno: Alumno }) {
           >
             🤓 Grammatik 📚
           </a>
+
+{/* */}
+          <a
+            href="https://deutsch.ariiben.com/schreiben"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="px-6 py-2 bg-slate-600 hover:bg-slate-700 rounded text-center w-56"
+          >
+            🧾 Schreiben
+          </a>
+
         </div>
 
         {/* Columna derecha */}

22a3a1047b7403813e56be7e7322571efceb62a9
AriiBen <ramirez.ariben@gmail.com>
2026-04-25 01:09:02 -0600

    add checkin

--- app/schueler/SchuelerClient.tsx
+++ app/schueler/SchuelerClient.tsx
@@ -84,11 +84,20 @@ export default function SchuelerClient({ alumno }: { alumno: Alumno }) {
           >
             🤓 Grammatik 📚
           </a>
+
+          <a
+            href="https://deutsch.ariiben.com/schueler/checkin"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="px-6 py-2 bg-slate-600 hover:bg-slate-700 rounded text-center w-56"
+          >
+            💫 CheckIn ✨
+          </a>
         </div>
 
         <div className="flex flex-col gap-4">
           <a
-            href="https://spaces.ariiben.com"
+            href="https://house-three-ochre.vercel.app/"
             target="_blank"
             rel="noopener noreferrer"
             className="px-6 py-2 bg-slate-600 hover:bg-slate-700 rounded text-center w-56"
